56fcfcc92d63c14c5add414ce81ae3a83b8a24d9
alzari555 <105596081+alzari555@users.noreply.github.com>
2025-11-19 19:29:35 -0300

    1.5.3

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -4,7 +4,7 @@ import { Linkedin, Instagram } from 'lucide-react';
 
 const Socials: React.FC = () => {
   return (
-    <section className="pt-0 pb-12 sm:pb-16 bg-[#1d1d1b]">
+    <section className="pt-12 pb-12 sm:pb-16 bg-[#1d1d1b]">
       <div className="container mx-auto px-6 text-center">
         <div className="flex flex-col sm:flex-row items-center sm:justify-center space-y-4 sm:space-y-0 sm:space-x-6">
           <a href="#" target="_blank" rel="noopener noreferrer" className="bg-gradient-to-r from-[#993399] to-[#3645a9] text-white font-bold py-3 px-8 rounded-full hover:scale-105 transition-transform duration-300 text-lg shadow-2xl shadow-[#993399]/30 inline-flex items-center">
